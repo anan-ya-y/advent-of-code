eee37280b230ca55dd4f85b11550923b9f4bef57
Ananya <14322650+ananyeet@users.noreply.github.com>
2023-12-05 09:24:43 -0600

    DAY 2

---
+++ day2.py
@@ -0,0 +1,51 @@
+import utils
+import re
+  
+# returns id, [red_values], [green_values], [blue_values]
+def get_game_info(line):
+    id = int(re.findall(r"Game (\d+):", line)[0])
+    gamestr = line.split(":")[-1].split(";")
+
+    red_values = []
+    green_values = []
+    blue_values = []
+    for round in gamestr:
+        r = re.findall(r"(\d+) red", round)
+        g = re.findall(r"(\d+) green", round)
+        b = re.findall(r"(\d+) blue", round)
+
+        r = int(r[0]) if r != [] else 0
+        g = int(g[0]) if g != [] else 0
+        b = int(b[0]) if b != [] else 0
+
+        red_values.append(r)
+        green_values.append(g)
+        blue_values.append(b)
+    return id, red_values, green_values, blue_values
+
+    
+
+def part1_solution(filename):
+    input = utils.read_file(filename)
+    sum = 0
+    for line in input:
+        id, reds, greens, blues = get_game_info(line)
+        if max(reds) <= 12 and \
+            max(greens) <= 13 and \
+            max(blues) <= 14:
+            sum += id
+    print(sum)
+
+def part2_solution(filename):
+    input = utils.read_file(filename)
+    sum = 0
+    for line in input:
+        id, reds, greens, blues = get_game_info(line)
+        this_game_value = max(reds) * max(greens) * max(blues)
+        sum += this_game_value
+    print(sum)
+        
+        
+
+part1_solution("inputs/2.real.txt")
+part2_solution("inputs/2.real.txt")
