612a5a54ca9594c7ee0b665f30d598260d4b5fa1
Ananya <14322650+ananyeet@users.noreply.github.com>
2024-01-05 16:23:07 -0600

    16p1

--- day16.py
+++ day16.py
@@ -1,6 +1,7 @@
 import utils, re
 
-# DP day! 
+# DP day!
+TIME = 30
 
 def read_input(input):
     input = utils.split_and_strip(input)
@@ -9,7 +10,7 @@ def read_input(input):
     for line in input:
         a = re.findall(r"Valve (\D+) has flow rate=(\d+); (tunnels|tunnel) (leads|lead) to (valves|valve) (\D+)", line)[0]
         key = a[0]
-        flowrate = a[1]
+        flowrate = int(a[1])
         children = [c.strip() for c in a[-1].split(",")]
         graph[key] = children
         flowrates[key] = flowrate
@@ -26,37 +27,34 @@ def mfr(current, minutenum, opened, graph, flowrates):
     # = max pressure from minute mn to minute 30 
     # given human start position at current. 
     # and visited contains all visited notdes
-    # print(current, minutenum, opened)
     if (current, minutenum, opened) in mfr_arr:
         return mfr_arr[(current, minutenum, opened)]
     
     opened = list(opened)
-    all_opened_flow = sum([int(flowrates[o]) for o in opened])
+    all_opened_flow = sum([flowrates[o] for o in opened])
 
-    if minutenum > 30:
+    if minutenum > TIME:
         return 0
 
-    if len(opened) == len(graph.keys()): # everything is open
-        return all_opened_flow
-    if len(opened) % 4 == 0:
-        pass
-        # print(current, minutenum, opened)
-
+    nonzero_flows = [f for f in flowrates if flowrates[f] != 0]
+    if len(set(nonzero_flows).difference(set(opened))) == 0: 
+        # Everything that matters is open. 
+        return all_opened_flow * (TIME - minutenum + 1)
 
     children = graph[current]
     options = []
+    # move to next valve
     for c in children:
-        # leave the valve as-is, just move to next valve
-        options.append(mfr(c, minutenum + 1, tuple(opened), graph, flowrates))
+        opt = mfr(c, minutenum + 1, tuple(opened), graph, flowrates)
+        options.append(opt)
 
-        # if closed, open the valve
-        if current not in opened and flowrates[current] != 0:
-            o = opened.copy()
-            o.append(current)
-            o.sort()
-            next = mfr(c, minutenum + 2, tuple(o), graph, flowrates)
-            next += all_opened_flow
-            options.append(next)
+    # if closed, open the valve
+    if current not in opened and flowrates[current] != 0:
+        o = opened.copy()
+        o.append(current)
+        o.sort()
+        next = mfr(current, minutenum + 1, tuple(o), graph, flowrates)
+        options.append(next)
 
     ans = max(options) + all_opened_flow
     mfr_arr[(current, minutenum, tuple(opened))] = ans
