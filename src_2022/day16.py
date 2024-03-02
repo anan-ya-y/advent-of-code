774995b6ab15767cbf098a2951e0ed580adb22eb
Ananya <14322650+ananyeet@users.noreply.github.com>
2024-03-02 13:16:30 -0600

    day 16 done

--- src_2022/day16.py
+++ src_2022/day16.py
@@ -1,6 +1,8 @@
 import utils, re
 # Thanks hyper-neutrino for the graph compression idea as well as how to do p2! 
 
+import time
+
 def read_input(input):
     input = utils.split_and_strip(input)
     flowrates = {}
@@ -17,9 +19,8 @@ def read_input(input):
 def compress_graph(graph, flowrates):
     good_valves = [v for v in flowrates if flowrates[v] != 0] + ["AA"]
     distances = {}
-    edge_function = lambda u, v: 1 if v in graph[u] else None
     for v1 in good_valves:
-        v1dists = utils.bfs(graph.keys(), edge_function, v1, None)
+        v1dists = utils.bfs_with_neighbors(graph.keys(), graph, v1, None)
         for v2 in good_valves:
             distances[(v1, v2)] = v1dists[v2]
 
@@ -34,19 +35,19 @@ def mfr(current, remaining_time, opened, flowrates, distances, bitmask_legend):
     if (current, remaining_time, opened) in mfr_arr:
         return mfr_arr[(current, remaining_time, opened)]
     
-    all_valves = set(bitmask_legend)
-    opened_set = utils.bitmask_to_set(opened, bitmask_legend)
-    unopened_valves = all_valves.difference(opened_set)
-
     max_flow = 0
-    for valve in unopened_valves:
+    for i in range(len(bitmask_legend)):
+        valve = bitmask_legend[i]
+        if (opened >> i) & 1: # this valve is already opened. 
+            continue
+
         # check if we can close it
         new_remtime = remaining_time - distances[(current, valve)] - 1
         if new_remtime <= 0:
             continue # we don't have time to open it. 
 
-        new_opened = utils.set_to_bitmask(opened_set.union({valve}), bitmask_legend)
-        # new_opened = opened | (1 << bitmask_legend.index(valve))
+        # new_opened = utils.set_to_bitmask(opened_set.union({valve}), bitmask_legend)
+        new_opened = opened | (1 << bitmask_legend.index(valve))
 
         flow = flowrates[valve] * new_remtime # we opened this valve. 
         flow += mfr(valve, new_remtime, new_opened, flowrates, distances, bitmask_legend)
@@ -56,9 +57,18 @@ def mfr(current, remaining_time, opened, flowrates, distances, bitmask_legend):
     mfr_arr[(current, remaining_time, opened)] = max_flow
     return max_flow
 
+def memoize(startlist, max_time, max_bitmask, flowrates, distances, bitmask_legend):
+    for start in startlist:
+        for t in range(max_time + 1, -1, -1):
+            for bitmask in range(max_bitmask+1, -1, -1):
+                mfr(start, t, bitmask, flowrates, distances, bitmask_legend)
+
 def p1(input):
     graph, flowrates = read_input(input)
     distances, good_valves = compress_graph(graph, flowrates)
+    # memoize(["AA"], 30, (1 << len(good_valves)) - 1, flowrates, distances, good_valves)
+    # print(time.time() - t  )
+    # print("done memoizing")
 
     x = mfr("AA", 30, 0, flowrates, distances, good_valves)
     return x
