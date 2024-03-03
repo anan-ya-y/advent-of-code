f70135f0f5e3f268bac6a467b582d096890614b3
Ananya <14322650+ananyeet@users.noreply.github.com>
2024-03-03 10:19:48 -0600

    day 19 is correct, just s l o w

--- src_2022/day19.py
+++ src_2022/day19.py
@@ -14,6 +14,7 @@ def p1(input):
 
         geodes = {}
         g = get_geodes(costs, [1, 0, 0, 0], [0, 0, 0, 0], 24)
+        print(bpnum, len(geodes.keys()), g)
         s += bpnum * g
     return s
 
@@ -23,18 +24,19 @@ def p2(input):
 
 def get_geodes(costs, robots, supply, time_left):
     tuple_index = tuple(robots + supply + [time_left])
+    # tuple_index = tuple([robots + supply + time_left])
     if tuple_index in geodes:
-        print(tuple_index, "found in geodes")
+        # print(tuple_index, "found in geodes")
         return geodes[tuple_index]
-    # print(robots, "\t", supply, "\t", time_left)
-    # print(costs)
+    
     # ore, clay, obsidian, geode
+    
     if time_left <= 0:
         return supply[-1]
     
     # if we have no time to make a geode robot
-    if time_left <= costs[3][1] and robots[2] == 0:
-        return 0
+    # if time_left < costs[3][1] and robots[2] == 0:
+    #     return 0
     
     max_spend = [
         max(costs[0], costs[1], costs[2][0], costs[3][0]), 
@@ -45,20 +47,18 @@ def get_geodes(costs, robots, supply, time_left):
         # return supply
         return [min(supply[i], max_spend[i]*time_left) for i in range(3)] + [supply[-1]]
         
-
     # robots just build
-    just_build = list(map(sum, zip(robots, supply)))
+    just_build = cap_supply(list(map(sum, zip(robots, supply))))
 
     # do not build anything
     best_geodes = get_geodes(costs, robots, just_build, time_left-1)
 
-    # build a geode robot - if you can do this, don't try anything else
+    # build a geode robot 
     if supply[0] >= costs[3][0] and supply[2] >= costs[3][1]:
         new_robots = list(map(sum, zip(robots, [0, 0, 0, 1])))
         new_supply = list(map(sum, zip(just_build, [-costs[3][0], 0, -costs[3][1], 0])))
         new_supply = cap_supply(new_supply)
         best_geodes = max(best_geodes, get_geodes(costs, new_robots, new_supply, time_left-1))
-        # return best_geodes
     
     # build an obsidian robot
     if supply[0] >= costs[2][0] and supply[1] >= costs[2][1]:
@@ -66,7 +66,6 @@ def get_geodes(costs, robots, supply, time_left):
         new_supply = list(map(sum, zip(just_build, [-costs[2][0], -costs[2][1], 0, 0])))
         new_supply = cap_supply(new_supply)
         best_geodes = max(best_geodes, get_geodes(costs, new_robots, new_supply, time_left-1))
-        # return best_geodes
     
     # build a clay robot
     if supply[0] >= costs[1] and robots[1] < max_spend[1]:
@@ -74,7 +73,6 @@ def get_geodes(costs, robots, supply, time_left):
         new_supply = list(map(sum, zip(just_build, [-costs[1], 0, 0, 0])))
         new_supply = cap_supply(new_supply)
         best_geodes = max(best_geodes, get_geodes(costs, new_robots, new_supply, time_left-1))
-        # return best_geodes
 
     # build an ore robot
     if supply[0] >= costs[0] and robots[0] < max_spend[0]:
@@ -82,7 +80,6 @@ def get_geodes(costs, robots, supply, time_left):
         new_supply = list(map(sum, zip(just_build, [-costs[0], 0, 0, 0])))
         new_supply = cap_supply(new_supply)
         best_geodes = max(best_geodes, get_geodes(costs, new_robots, new_supply, time_left-1))
-        # return best_geodes
     
     geodes[tuple_index] = best_geodes
     return best_geodes
