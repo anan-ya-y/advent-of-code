2f8ce26db8991a250eaccf3a3a45d62d3e301f28
Ananya <14322650+ananyeet@users.noreply.github.com>
2024-06-30 11:27:50 -0500

    22, FASTER

--- src_2016/day22.py
+++ src_2016/day22.py
@@ -7,9 +7,14 @@ adjacent = lambda x, y: (abs(x.real - y.real) <= 1 and \
                             x != y)
 swappable = lambda x, y, grid: x in grid and \
                                 y in grid and \
-                                grid[x]["used"] <= grid[y]["avail"] and \
+                                grid[x][USED] <= grid[y][AVAIL] and \
                                 x != y
 valid = lambda x, y, grid: adjacent(x, y) and swappable(x, y, grid)
+
+SIZE=0
+USED=1
+AVAIL=2
+USE=3
 
 def print_grid(grid):
     max_x = int(max([k.real for k in grid]))
@@ -19,7 +24,7 @@ def print_grid(grid):
         for j in range(max_x+1):
             node = c(j, i)
             if node in grid:
-                print(f"{grid[node]['used']}/{grid[node]['size']}", end=" ")
+                print(f"{grid[node][USED]}/{grid[node][SIZE]}", end=" ")
             else:
                 print(" ", end="")
         print()
@@ -32,19 +37,14 @@ def parse_input(input):
     for line in input[2:]:        
         k = re.search(regex_expression, line)
         k = list(map(int, k.groups()))
-        grid[c(k[0], k[1])] = {
-            "size": k[2],
-            "used": k[3],
-            "avail": k[4],
-            "use_percent": k[5]
-        }
+        grid[c(k[0], k[1])] = k[2:]
     return grid
 
 def execute_swaps(grid, moves):
     for i in range(len(moves)-1):
         current, next = moves[i], moves[i+1]
-        grid[current]["used"] += grid[next]["used"]
-        grid[next]["used"] = 0
+        grid[current][USED] += grid[next][USED]
+        grid[next][USED] = 0
     return grid
 
 def p1(input):
@@ -52,7 +52,7 @@ def p1(input):
     viable_pairs  = 0
     for a in grid:
         for b in grid:
-            if grid[a]["used"] != 0 and swappable(a, b, grid):
+            if grid[a][USED] != 0 and swappable(a, b, grid):
                 viable_pairs += 1
     return viable_pairs
 
@@ -63,14 +63,14 @@ def p2(input):
     # print_grid(grid)
 
     # Find the empty node
-    empty = [k for k, v in grid.items() if v["used"] == 0][0]
+    empty = [k for k, v in grid.items() if v[USED] == 0][0]
     target = max([k for k in grid if k.imag == 0], key=lambda x: x.real)
     # print(f"EMPTYLOC: {empty}, TARGETLOC: {target}")
 
     # move the empty to right next to the target
     def neighbor_generator(node):
         position, swaps = node
-        new_grid = execute_swaps(copy.deepcopy(grid), swaps)
+        new_grid = execute_swaps(grid.copy(), swaps)
         # print(f"after execution of {swaps}")
         # print_grid(new_grid)
         directions = [c(1, 0), c(-1, 0), c(0, 1), c(0, -1)]
@@ -94,15 +94,15 @@ def p2(input):
 
 
     def toptworows_swappable(grid): # the data of any node can fit in any node
-        max_data = max([grid[v]["used"] for v in grid if v.imag <= 1])
-        min_capacity = min([grid[v]["size"] for v in grid if v.imag <= 1])
+        max_data = max([grid[v][USED] for v in grid if v.imag <= 1])
+        min_capacity = min([grid[v][SIZE] for v in grid if v.imag <= 1])
         return max_data <= min_capacity
     assert toptworows_swappable(grid), "Top two rows not universally swappable"
 
     nsteps += 1 # swap so grid has .G_ instead of ._G
     execute_swaps(grid, [target-c(1, 0), target])
     # empty should now be in the position of the target
-    assert grid[target]["used"] == 0, "Empty node not in target position"
+    assert grid[target][USED] == 0, "Empty node not in target position"
 
     # it takes 5 turns to go from .G_ to G_.
     nmoves_g = target.real# ...G_ -> G_... number of moves
