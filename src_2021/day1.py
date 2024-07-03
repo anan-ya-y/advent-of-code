e082c9656b9b680be335acef1da92179d063f82e
Ananya <14322650+ananyeet@users.noreply.github.com>
2024-07-03 17:15:16 -0500

    1

--- src_2021/day1.py
+++ src_2021/day1.py
@@ -9,4 +9,10 @@ def main(input):
         if input[i+1] > input[i]:
             p1 += 1
 
+    for i in range(len(input) - 2):
+        sum1 = sum(input[i:i+3])
+        sum2 = sum(input[i+1:i+4])
+        if sum2 > sum1:
+            p2 += 1
+
     return p1, p2
