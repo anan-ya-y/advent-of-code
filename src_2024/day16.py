7c0852277c491e903e54c7e631d75df294574bd1
Ananya <14322650+ananyeet@users.noreply.github.com>
2024-12-18 10:44:49 -0600

    16 clean

--- src_2024/day16.py
+++ src_2024/day16.py
@@ -5,42 +5,12 @@ c = complex
 DIRECTIONS = [c(0, 1), c(0, -1), c(1, 0), c(-1, 0)]
 EAST = c(0, 1)
 
-def path_cost(path):
-    dir = path[1] - path[0]
-    nturns = 1 if dir != EAST else 0
-    # breakpoint()
-    for i in range(len(path)-1):
-        new_dir = path[i+1] - path[i]
-        if new_dir != dir:
-            nturns += 1
-            dir = new_dir
-
-    return 1000*nturns + len(path) - 1
-
-def path_cost_dijkstra(path):
-    # if len(set([p[0] for p in path])) != len(path):
-        # return float('inf')
-    ans = 0
-    for i in range(len(path)-1):
-        ans += edge_fn(path[i], path[i+1])
-    return ans
-
 def edge_fn(u, v):
     up, ud = u
     vp, vd = v
     if up == "TARGET" or vp == "TARGET":
         return 0
     return 1 if ud == vd else 1001
-
-
-# _cached reduces the runtime SIGNIFICANTLY
-visited_cached_fwd = set()
-visited_cached_bwd = set()
-def visited_fn(v, visited, visited_cached):
-    if v[0] in visited_cached:
-        return True
-    visited_cached.add(v[0])
-    return False
 
 def plot_path(inp_map, path):
     xrange = max([int(p.real) for p in inp_map])
@@ -55,34 +25,6 @@ def plot_path(inp_map, path):
                 print(inp_map[p], end="")
         print()
 
-# unfortunately today I am writing my own BFS.. 
-def bfs(start_vertex, neighbor_fn, target=None):
-
-    q = []
-    q.append((start_vertex, [start_vertex]))
-    paths = []
-    visited = {}
-
-    while len(q) > 0:
-        u, path = q.pop(0)
-
-        # if len(path) > 2:
-        #     pc = path_cost(path)
-
-        #     if u in visited and pc > visited[u]:
-        #         continue
-        #     visited[u] = pc
-        # else:
-        #     visited[u] = 1
-
-        if u == target:
-            paths.append(path)
-        else:
-            for v in neighbor_fn((u, path)):
-                q.append((v, path + [v]))
-
-    return paths, visited
-
 def combine_paths(fwd_path, bwd_path):
     bwd_path = [(p[0], -1*p[1]) for p in bwd_path[::-1]]
     return fwd_path[:-1] + bwd_path
@@ -95,7 +37,6 @@ def main(inp):
 
     TARGET_NODE = ("TARGET", c(0, 0))
 
-    
     def neighbor_fn(node):
         u, ud = node
 
@@ -114,16 +55,14 @@ def main(inp):
     
     dist, paths = utils.dijkstra_with_generators((start, EAST), \
                                                     neighbor_fn, edge_fn, \
-                lambda v, visited: visited_fn(v, visited, visited_cached_fwd))
+                                                    set)
 
     dists_from_end, paths_end = utils.dijkstra_with_generators(TARGET_NODE, \
                                                     neighbor_fn, edge_fn, 
-                lambda v, visited: visited_fn(v, visited, visited_cached_bwd))
+                                                    set)
 
     p1 = dist[TARGET_NODE]
     p2 = set()
-
-
     for k in dist:
         k_reverse = (k[0], k[1]*-1)
         if k_reverse not in dists_from_end:
@@ -141,30 +80,5 @@ def main(inp):
     
     p2.remove("TARGET")
 
-    plot_path(inp_map, p2)
-    return p1, len(p2)
-
-
-    exit()
-
-    def neighbors_normal(node):
-        u, path = node
-        nbrs = []
-        for d in DIRECTIONS:
-            new_u = u + d
-            if new_u in inp_map and inp_map[new_u] != "#" and new_u not in path:
-                nbrs.append(new_u)
-        return nbrs
-
-    allish_paths, visited = bfs(start, neighbors_normal, end)
-    path_costs = [path_cost(p) for p in allish_paths]
-
-
-    p1 = min(path_costs)
-    p2 = set()
-    for p in allish_paths:
-        if path_cost(p) == p1:
-            p2 = p2.union(set(p))
-    plot_path(inp_map, p2)
-
+    # plot_path(inp_map, p2)
     return p1, len(p2)
