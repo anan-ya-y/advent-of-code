691b38dfd20af40d5e16b39f2f8a363bb1293e1a
Ananya Y <14322650+anan-ya-y@users.noreply.github.com>
2025-06-21 12:46:34 -0500

    add reqs.txt, create inputs/ dir

--- runner_utils.py
+++ runner_utils.py
@@ -22,6 +22,8 @@ def get_input_filename(year, day, sample=False):
         print("file exists. opening. ")
         return filepath
     elif not sample:
+        if not os._exists("src_" + str(year) + "/inputs/"):
+            os.makedirs("src_" + str(year) + "/inputs/")
         with open(filepath, "w") as f:
             f.write(aocd.get_data(day=day, year=year))
     else:
