8005d8067d33e82ded4648f57f27b84521e8f1ea
Ananya Y <14322650+anan-ya-y@users.noreply.github.com>
2025-06-21 17:24:19 -0500

    11 completed MUCH faster...

--- src_2016/day11.py
+++ src_2016/day11.py
@@ -1,65 +1,34 @@
-import re, utils
-from itertools import permutations
+'''
+.
+.
+HM
+LG, LM, MG
 
-ALL_OBJS = []
-ALL_ELEMS = []
-MAX_FLOOR = 3
-MIN_FLOOR = 0
+is the same state as
+
+LM
+HG, HM, LG
+'''
+
+import utils
 
 # floor representation: List of sets. 
 # floor+elevator representation = tuple(flors, elevatorpos)
 def read_input(input):
-    input = utils.split_and_strip(input)
-    floors = []
-    for i in range(3):
-        floors.append(set(input[i].split(", ")))
-    floors.append(set())
-    return floors
+    state = [
+        ["PG", "PM"],
+        ["OG", "UG", "RG", "LG"],
+        ["OM", "UM", "RM", "LM"], 
+        []
+    ]
 
-def get_all_objects(floors):
-    global ALL_OBJS, ALL_ELEMS
-    ALL_OBJS = set.union(*floors)
-    ALL_ELEMS = list(set([item[0] for item in ALL_OBJS]))
-    return ALL_OBJS
-
-def state_to_string(state):
-    floors, elevator = state
-    floors = [sorted(floor) for floor in floors]
-    return f"{str(floors)}-{elevator}"
-
-def serialize(state):
-    floors, e = state
-    serial = []
-    for f in floors:
-        m = [item for item in f if item[1] == "M"]
-        g = [item for item in f if item[1] == "G"]
-        serial.append((len(m), len(g)))
-    return tuple([tuple(serial), e])
-
-perms = {}
-def get_all_perms(state):
-    floorstate, e = state
-    all_perms = permutations(ALL_ELEMS)
-    all_permstrings = set()
-    s = state_to_string(state)
-    if s in perms:
-        return perms[s]
-    for perm in all_perms:
-        newstate = []
-        for floor in floorstate:
-            newfloor = set()
-            for item in floor:
-                itemindex = ALL_ELEMS.index(item[0])
-                newitem = f"{perm[itemindex]}{item[1]}"
-                newfloor.add(newitem)
-            newstate.append(newfloor)
-        all_permstrings.add(state_to_string((newstate, e)))
-    perms[s] = all_permstrings
-    return all_permstrings
-
-def equivalent_states(s1, s2):
-    perms = get_all_perms(s1)
-    return state_to_string(s2) in perms
+    sample_state = [
+        ["HM", "LM"], 
+        ["HG"], 
+        ["LG"], 
+        []
+    ]
+    return state, 0
 
 def state_valid(floors):
     for floor in floors:
@@ -77,70 +46,86 @@ def state_valid(floors):
                 return False
     return True
 
-def get_next_states(state):
+def serialize(state):
     floors, e = state
-    # print( "floors", floors)
+    s = []
+    for f in floors:
+        m = [item for item in f if item[1] == "M"]
+        g = [item for item in f if item[1] == "G"]
+        s.append(len(m))
+        s.append(len(g))
+
+    s.append(e)
+    return tuple(s)
+
+def is_end_state(state):
+    floors, e = state
+    all_but_last = []
+    for f in floors[:-1]:
+        all_but_last.extend(f)
+    return len(all_but_last) == 0
+
+def get_next_states(state):
     next_states = []
-    for new_e in [e+1, e-1]:
-        if new_e > MAX_FLOOR or new_e < MIN_FLOOR:
-            continue
+    floors, e = state
+
+    next_floors = []
+    if e > 0:
+        next_floors.append(e-1)
+    if e < len(floors)-1:
+        next_floors.append(e+1)
+
+    for f in next_floors:
         for item1 in floors[e]:
-            for item2 in list(floors[e]) + ["--"]:
+            for item2 in list(floors[e] + ["--"]):
                 if item1 == item2:
                     continue
+                if item2 != "--" and item2 > item1:
+                    continue
 
-                new_floors = [f.copy() for f in floors]
+                new_floors = [x.copy() for x in floors]
 
                 new_floors[e].remove(item1)
-                new_floors[new_e].add(item1)
-                if item2 != "--":
+                new_floors[f].append(item1)
+                if item2 != '--':
                     new_floors[e].remove(item2)
-                    new_floors[new_e].add(item2)
+                    new_floors[f].append(item2)
 
                 if state_valid(new_floors):
-                    next_states.append((new_floors, new_e))
+                    next_states.append((new_floors, f))
+
     return next_states
 
-def get_min_elevator(start_position, target_position):
-    seen_states = set() # set of state strings. 
-    q = [(start_position, 0)]
-    target_states = get_all_perms(target_position)
+def get_min_elevator(start_position):
+    seen_states = set()
+    q = [(start_position, 0, [])]
 
     while len(q) > 0:
-        state, steps = q.pop(0)
-        state_str = state_to_string(state)
-        s = serialize(state)
-        if s in seen_states:
+        state, steps, seq = q.pop(0)
+        serialized_state = serialize(state)
+        if serialized_state in seen_states:
             continue
-        seen_states.add(s)
 
-        if state_str in target_states:
+        seen_states.add(serialized_state)
+
+        if is_end_state(state):
             return steps
+        
         for s in get_next_states(state):
-            q.append((s, steps+1))
+            sc = seq.copy()
+            sc.append(s)
+            q.append((s, steps+1, sc))
 
     return -1
 
+    
 def p1(input):
-    floors = read_input(input)
-    all_stuff = get_all_objects(floors)
-
-    end_floors = [set()] * (MAX_FLOOR) + [all_stuff]
-    end_position = (end_floors, MAX_FLOOR)
-    start_position = (floors, 0)
-
-    nsteps = get_min_elevator(start_position, end_position)
-    return nsteps
+    start_state = read_input(input)
+    return get_min_elevator(start_state)
 
 def p2(input):
-    floors = read_input(input)
-    lobby = set(["DG", "DM", "EG", "EM"])
-    floors[0] = floors[0].union(lobby)
-    all_stuff = get_all_objects(floors)
+    start_state = read_input(input)
+    start_state[0][0] += ["DG", "DM", "EG", "EM"]
+    return get_min_elevator(start_state)
+    
 
-    end_floors = [set()] * (MAX_FLOOR) + [all_stuff]
-    end_position = (end_floors, MAX_FLOOR)
-    start_position = (floors, 0)
-
-    nsteps = get_min_elevator(start_position, end_position)
-    return nsteps
--- src_2016/day11_old.py
+++ src_2016/day11_old.py
@@ -1,216 +1,146 @@
-import utils, copy
-from queue import PriorityQueue
+import re, utils
 from itertools import permutations
 
+ALL_OBJS = []
+ALL_ELEMS = []
+MAX_FLOOR = 3
+MIN_FLOOR = 0
+
+# floor representation: List of sets. 
+# floor+elevator representation = tuple(flors, elevatorpos)
 def read_input(input):
     input = utils.split_and_strip(input)
     floors = []
     for i in range(3):
-        floors.append(input[i].split(", "))
-    floors.append([])
-    floors = [tuple(sorted(f)) for f in floors]
-    return tuple(floors)
+        floors.append(set(input[i].split(", ")))
+    floors.append(set())
+    return floors
 
-# not checking if all the items are still there...
-def is_valid_move(state1, state2):
-    def is_valid_floor(floor):
+def get_all_objects(floors):
+    global ALL_OBJS, ALL_ELEMS
+    ALL_OBJS = set.union(*floors)
+    ALL_ELEMS = list(set([item[0] for item in ALL_OBJS]))
+    return ALL_OBJS
+
+def state_to_string(state):
+    floors, elevator = state
+    floors = [sorted(floor) for floor in floors]
+    return f"{str(floors)}-{elevator}"
+
+def serialize(state):
+    floors, e = state
+    serial = []
+    for f in floors:
+        m = [item for item in f if item[1] == "M"]
+        g = [item for item in f if item[1] == "G"]
+        serial.append((len(m), len(g)))
+    return tuple([tuple(serial), e])
+
+perms = {}
+def get_all_perms(state):
+    floorstate, e = state
+    all_perms = permutations(ALL_ELEMS)
+    all_permstrings = set()
+    s = state_to_string(state)
+    if s in perms:
+        return perms[s]
+    for perm in all_perms:
+        newstate = []
+        for floor in floorstate:
+            newfloor = set()
+            for item in floor:
+                itemindex = ALL_ELEMS.index(item[0])
+                newitem = f"{perm[itemindex]}{item[1]}"
+                newfloor.add(newitem)
+            newstate.append(newfloor)
+        all_permstrings.add(state_to_string((newstate, e)))
+    perms[s] = all_permstrings
+    return all_permstrings
+
+def equivalent_states(s1, s2):
+    perms = get_all_perms(s1)
+    return state_to_string(s2) in perms
+
+def state_valid(floors):
+    for floor in floors:
         microchips = [item for item in floor if item[1] == "M"]
         generators = [item for item in floor if item[1] == "G"]
 
         # if there are no generators, then the microchips are safe
         if len(generators) == 0:
-            return True
+            continue
         
         # okay, there's a generator. 
         # make sure all microchips have partner generators
         for microchip in microchips:
             if f"{microchip[0]}G" not in generators:
                 return False
-        return True
-
-    start_floors, start_e = state1
-    end_floors, end_e = state2
-
-    # elevator has only moved one floor
-    if abs(start_e - end_e) != 1:
-        return False
-    
-    # elevator has moved to a valid floor
-    # if end_e < 0 or end_e > 3:
-    #     return False
-    for floor in end_floors:
-        if not is_valid_floor(floor):
-            return False
     return True
 
-def get_all_objects(floors):
-    objects = []
-    for floor in floors:
-        for item in floor:
-            if item not in objects:
-                objects.append(item)
-    return objects
-
-def get_all_elements(floors):
-    elements = []
-    for f in floors:
-        for item in f:
-            if item[0] not in elements:
-                elements.append(item[0])
-    return elements
-
-# floors is a tuple of tuples. 
-def get_next_states(state, lobby=False):
+def get_next_states(state):
     floors, e = state
+    # print( "floors", floors)
     next_states = []
-    # assume that two different stuff cant be in elevator. 
+    for new_e in [e+1, e-1]:
+        if new_e > MAX_FLOOR or new_e < MIN_FLOOR:
+            continue
+        for item1 in floors[e]:
+            for item2 in list(floors[e]) + ["--"]:
+                if item1 == item2:
+                    continue
 
-    floors_tol = tuple([list(f) for f in floors])
-    floor_contents = list(floors[e])
+                new_floors = [f.copy() for f in floors]
 
-    for i in range(len(floor_contents)):
-        # move one item. 
-        floor_up = copy.deepcopy(floors_tol)
-        floor_up[e].remove(floor_contents[i])
-        floor_down = copy.deepcopy(floor_up)
-        if (e < 3 and not lobby) or (e < 4 and lobby):
-            floor_up[e+1].append(floor_contents[i])
-            up_tuple = tuple([tuple(sorted(f)) for f in floor_up])
-            if is_valid_move((floors, e), (up_tuple, e+1)):
-                next_states.append((up_tuple, e+1))
-        if e > 0:
-            floor_down[e-1].append(floor_contents[i])
-            down_tuple = tuple([tuple(sorted(f)) for f in floor_down])
-            if is_valid_move((floors, e), (down_tuple, e-1)):
-                next_states.append((down_tuple, e-1))
-        
-        # move two items
-        for j in range(i+1, len(floor_contents)):
-            floor_up = copy.deepcopy(floors_tol)
-            floor_up[e].remove(floor_contents[j])
-            floor_up[e].remove(floor_contents[i])
-            floor_down = copy.deepcopy(floor_up)
-            if (e < 3 and not lobby) or (e < 4 and lobby):
-                floor_up[e+1].append(floor_contents[j])
-                floor_up[e+1].append(floor_contents[i])
-                up_tuple = tuple([tuple(sorted(f)) for f in floor_up])
-                if is_valid_move((floors, e), (up_tuple, e+1)):
-                    next_states.append((up_tuple, e+1))
-            if e>0:
-                floor_down[e-1].append(floor_contents[j])
-                floor_down[e-1].append(floor_contents[i])
-                down_tuple = tuple([tuple(sorted(f)) for f in floor_down])
-                if is_valid_move((floors, e), (down_tuple, e-1)):
-                    next_states.append((down_tuple, e-1))
+                new_floors[e].remove(item1)
+                new_floors[new_e].add(item1)
+                if item2 != "--":
+                    new_floors[e].remove(item2)
+                    new_floors[new_e].add(item2)
 
-    # print(state)
-    # for s in next_states:
-    #     print(s)
-    # print("---")
+                if state_valid(new_floors):
+                    next_states.append((new_floors, new_e))
     return next_states
 
-seralized_dict = {}
-def seralize_floors(flr):
-    seen_elements = []
-    new_repn = []
-    for f in flr:
-        this_flr = []
-        for item in f:
-            if item[0] not in seen_elements:
-                seen_elements.append(item[0])
-            index = seen_elements.index(item[0])
-            this_flr.append(f"{index}{item[1]}")
-        new_repn.append(tuple(sorted(this_flr)))
-    return tuple(new_repn)
+def get_min_elevator(start_position, target_position):
+    seen_states = set() # set of state strings. 
+    q = [(start_position, 0)]
+    target_states = get_all_perms(target_position)
 
-def get_all_perms(state, elements):
-    floor, e = state
-    def get_perm(serialized, p):
-        new_flr = []
-        for f in serialized:
-            new_f = []
-            for item in f:
-                index = int(item[0])
-                new_f.append(f"{p[index]}{item[1]}")
-            new_flr.append(tuple(sorted(new_f)))
-        return tuple(new_flr)
-
-    serialized = seralize_floors(floor)
-    all_permutations = list(permutations(elements))
-    return [(get_perm(serialized, p), e) for p in all_permutations]
-
-def priority_fn(state):
-    floor, _ = state
-    p = 1
-    for i in range(len(floor)):
-        p += (10**i) * len(floor[i])
-    return -1*p
-        
-def custom_bfs(neighbor_generator, start_vertex, target=None, \
-                                priorityfn=None):
-    # q = [(start_vertex, 0)]
-    q = PriorityQueue()
-    queue_counter = 0 # pq tiebreaker. 
-    q.put((1 if priorityfn is None else priorityfn(start_vertex), \
-           queue_counter, start_vertex, 0))
-    queue_counter += 1 
-    dists = {}
-
-    elements = get_all_elements(start_vertex[0])
-
-    # while len(q) > 0:
-    while not q.empty():
-        # u, d = q.pop(0)
-        _, _, u, d = q.get()
-
-        if u in dists:
+    while len(q) > 0:
+        state, steps = q.pop(0)
+        state_str = state_to_string(state)
+        s = serialize(state)
+        if s in seen_states:
             continue
+        seen_states.add(s)
 
-        dists[u] = d
-        all_perms = get_all_perms(u, elements)
-        for p in all_perms:
-            if p not in dists:
-                dists[p] = d
+        if state_str in target_states:
+            return steps
+        for s in get_next_states(state):
+            q.append((s, steps+1))
 
-        if q.qsize() % 100 == 0:
-            print(q.qsize(), d)
-
-        if target is not None and u in get_all_perms(target, elements):
-            return dists[u]
-        for v in neighbor_generator(u):
-            if not v in dists: #state_in_list(v, list(dists.keys()), elements):
-                # q.append((v, d+1))
-                q.put((1 if priorityfn is None else priorityfn(v), \
-                       queue_counter, v, d+1))
-                queue_counter += 1
-
-    if target is None:
-        return dists
     return -1
 
 def p1(input):
     floors = read_input(input)
-    
     all_stuff = get_all_objects(floors)
-    end_list = [[], [], [], sorted(all_stuff)]
-    end_position = (tuple([tuple(f) for f in end_list]), 3)
+
+    end_floors = [set()] * (MAX_FLOOR) + [all_stuff]
+    end_position = (end_floors, MAX_FLOOR)
     start_position = (floors, 0)
-    x = custom_bfs(get_next_states, start_position, end_position)#, priorityfn=priority_fn)#, \
-                                        #    state_in_list=state_in_list)
-    return x
+
+    nsteps = get_min_elevator(start_position, end_position)
+    return nsteps
 
 def p2(input):
-    global perms
-    perms = {}
-
     floors = read_input(input)
-    lobby = ["DG", "DM", "EG", "EM"]
-    floors = tuple([tuple(sorted(f)) for f in [lobby] + list(floors)])
-    
+    lobby = set(["DG", "DM", "EG", "EM"])
+    floors[0] = floors[0].union(lobby)
     all_stuff = get_all_objects(floors)
-    end_list = [[], [], [], [], sorted(all_stuff)]
-    end_position = (tuple([tuple(f) for f in end_list]), 4)
+
+    end_floors = [set()] * (MAX_FLOOR) + [all_stuff]
+    end_position = (end_floors, MAX_FLOOR)
     start_position = (floors, 0)
-    x = custom_bfs(get_next_states, start_position, end_position)#, \
-                                        #    state_in_list=state_in_list)
-    return x
+
+    nsteps = get_min_elevator(start_position, end_position)
+    return nsteps
