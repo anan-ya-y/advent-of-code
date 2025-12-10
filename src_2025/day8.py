0905941597b0bc0bdae8670d59d53a1a1cf5b9d1
Ananya Y <14322650+anan-ya-y@users.noreply.github.com>
2025-12-10 13:15:52 -0600

    day 8

--- src_2025/day8.py
+++ src_2025/day8.py
@@ -12,11 +12,10 @@ def log2insert(arr, val, coords):
     if len(arr) == 0:
         return [(val, *coords)]
     if arr[-1][0] < val:
-        if len(arr) < 1000:
-            arr.append((val, *coords))
+        arr.append((val, *coords))
         return arr
     if arr[0][0] > val:
-        return [(val, *coords)] + (arr[:-1] if len(arr) >= 1000 else arr)
+        return [(val, *coords)] + arr
     
     low = 0
     high = len(arr) - 1
@@ -28,13 +27,36 @@ def log2insert(arr, val, coords):
             high = mid
 
     arr = arr[:low] + [(val, *coords)] + arr[low:]
-    return arr[:1000]
+    return arr[:10000] # arbitrarily choosing 4k... I hope that's enough for 1 component
 
 
+def add_connection(graph, d):
+    _, a, b = d
+    if a not in graph:
+        graph[a] = []
+    if b not in graph:
+        graph[b] = []
+
+
+    graph[a].append(b)
+    graph[b].append(a)
+
+    return graph
+
+def in_same_component(a, b, components):
+    a_comp = None
+    b_comp = None
+    for i in range(len(components)):
+        comp = components[i]
+        if a in comp:
+            a_comp = i
+        if b in comp:
+            b_comp = i
+
+    return a_comp == b_comp
     
-    
 
-def p1(inp):
+def main(inp):
     inp = utils.split_and_strip(inp)
     inp = [np.array(list(map(int, line.split(",")))) for line in inp]
 
@@ -42,29 +64,32 @@ def p1(inp):
     for i in range(len(inp)):
         for j in range(i, len(inp)):
             d = dist(inp[i], inp[j]) if i != j else float('inf')
-            dists = log2insert(dists, d, (i, j))
+            # dists = log2insert(dists, d, (i, j))
+            dists.append((d, i, j))
+
+    dists.sort(key=lambda x: x[0])
 
     graph = {} # vertex, [nbrs]
-    for d in (dists[:10] if len(inp) < 50 else dists):
-        _, a, b = d
-        if a not in graph:
-            graph[a] = []
-        if b not in graph:
-            graph[b] = []
+    p1_connections = 10 if len(inp) < 50 else 1000
+    for d in dists[:p1_connections]:
+        graph = add_connection(graph, d)
 
-
-        graph[a].append(b)
-        graph[b].append(a)
-
-    all_vertices = set(graph.keys())
-    components = []
-    while len(all_vertices) > 0:
-        start = list(all_vertices)[0]
-        reachable = utils.reachability(start, graph)
-        components.append(reachable)
-        all_vertices = all_vertices.difference(reachable)
+    components = utils.connected_components(graph)
 
     component_sizes = [len(x) for x in components]
     component_sizes.sort(reverse=True)
-    return component_sizes[0] * component_sizes[1] * component_sizes[2]
+    p1 = component_sizes[0] * component_sizes[1] * component_sizes[2]
 
+    for i in range(p1_connections, len(dists)):
+        indices = dists[i][1], dists[i][2]
+
+        if in_same_component(indices[0], indices[1], components):
+            continue
+
+        graph = add_connection(graph, dists[i])
+        components = utils.connected_components(graph)
+        if len(components) == 1:
+            p2 = inp[indices[0]][0]* inp[indices[1]][0]
+            break
+
+    return p1, p2
